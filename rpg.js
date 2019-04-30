3559797dee096a135d504493120f753a47813c25
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2019-04-30 11:41:03 -0300

    If -> Switch part 1

--- rpg.js
+++ rpg.js
@@ -20,72 +20,75 @@ client.on('message', (mensagem) => {
 		let splitCommand = fullCommand.split(" ") 
 		let primaryCommand = splitCommand[0]
 		
-		if(primaryCommand == "help") {
-			mensagem.channel.send("!intro\n!drive\n!trello\n!slack\n!savepauta (seguido da pauta a ser salva)\n!pauta\n!suggestion (seguido da sugestão a ser enviada)\n!yaharo\n!game (numero)\n!try (numero)")
-		}
-		if(primaryCommand == "drive") {
-			mensagem.channel.send("https://drive.google.com/drive/folders/0B0sTPCw3EupvVzllaEkyZDdYWWc")
-		}
-		if(primaryCommand == "trello") {
-			mensagem.channel.send("https://trello.com/rpgriopucgames/home")
-		}
-		if(primaryCommand == "slack") {
-			mensagem.channel.send("https://rpgriopucgames.slack.com/messages/C0QKLKN5V/details/")
-		}
-		if(primaryCommand == "savepauta") {
-			if(splitCommand[1] == undefined) {
-				mensagem.channel.send("Qual pauta você gostaria de salvar?")
-			}
-			else {
-				pauta = mensagem.content.substr(11)
-				mensagem.channel.send("Pauta Salva")
-			}
-		}
-		if(primaryCommand == "pauta") {
-			if(pauta == undefined) {
-				mensagem.channel.send("Não há nenhuma pauta salva.")
-			}
-			else {
-				mensagem.channel.send(pauta)
-			}
-		}
-		if(primaryCommand == "intro") {
-			mensagem.channel.send("Yaharo! Meu nome é RPG-chan e eu sou a Bot da RPG, espero que possamos trabalhar e vivenciar momentos infinitos juntos!\n\n!help")
-		}
-		if(primaryCommand == "suggestion") {
-			client.users.get("267749503635816462").send(mensagem.content.substr(12) + "\n\nEnviado por " + mensagem.author.username + "\n----------------------------------")
-		}
-		if(primaryCommand == "yaharo") {
-			mensagem.channel.send("https://myanimelist.net/anime/14813/Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru")
-		}
-		if(primaryCommand == "game") {
-			if(isNaN(parseInt(splitCommand[1],10))) {
-				mensagem.channel.send("Baka! Você esqueceu de informar o número")
-			}
-			else {
-				game = Math.floor((Math.random() * parseInt(splitCommand[1],10)) + 1)
-				mensagem.channel.send("Gemu starto!")
-				gameon = 1
-			}
-		}
-		if(primaryCommand == "try") {
-			if(gameon) {
-				if(isNaN(parseInt(splitCommand[1],10))) {
-					mensagem.channel.send("Baka! Você esqueceu de informar o número")
-					return
-				}
-				if(parseInt(splitCommand[1],10) == game) {
-					mensagem.channel.send("Yata, " + mensagem.author.username + " Acertou!")
-					gameon = 0
+		switch(primaryCommand) {
+			case "help":
+				mensagem.channel.send("!intro\n!drive\n!trello\n!slack\n!savepauta (seguido da pauta a ser salva)\n!pauta\n!suggestion (seguido da sugestão a ser enviada)\n!yaharo\n!game (numero)\n!try (numero)")
+				break
+			case "drive": 
+				mensagem.channel.send("https://drive.google.com/drive/folders/0B0sTPCw3EupvVzllaEkyZDdYWWc")
+				break
+			case "trello": 
+				mensagem.channel.send("https://trello.com/rpgriopucgames/home")
+				break
+			case "slack": 
+				mensagem.channel.send("https://rpgriopucgames.slack.com/messages/C0QKLKN5V/details/")
+				break
+			case "savepauta": 
+				if(splitCommand[1] == undefined) {
+					mensagem.channel.send("Qual pauta você gostaria de salvar?")
 				}
 				else {
-					mensagem.channel.send("Chigau!")
+					pauta = mensagem.content.substr(11)
+					mensagem.channel.send("Pauta Salva")
 				}
-			}
-			else {
-				mensagem.channel.send("O jogo ainda não começou!")
-			}
-			
+				break
+			case "pauta": 
+				if(pauta == undefined) {
+					mensagem.channel.send("Não há nenhuma pauta salva.")
+				}
+				else {
+					mensagem.channel.send(pauta)
+				}
+				break
+			case "intro": 
+				mensagem.channel.send("Yaharo! Meu nome é RPG-chan e eu sou a Bot da RPG, espero que possamos trabalhar e vivenciar momentos infinitos juntos!\n\n!help")
+				break
+			case "suggestion": 
+				client.users.get("267749503635816462").send(mensagem.content.substr(12) + "\n\nEnviado por " + mensagem.author.username + "\n----------------------------------")
+				break
+			case "yaharo": 
+				mensagem.channel.send("https://myanimelist.net/anime/14813/Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru")
+				break
+			case "game": 
+				if(isNaN(parseInt(splitCommand[1],10))) {
+					mensagem.channel.send("Baka! Você esqueceu de informar o número")
+				}
+				else {
+					game = Math.floor((Math.random() * parseInt(splitCommand[1],10)) + 1)
+					mensagem.channel.send("Gemu starto!")
+					gameon = 1
+				}
+				break
+			case "try": 
+				if(gameon) {
+					if(isNaN(parseInt(splitCommand[1],10))) {
+						mensagem.channel.send("Baka! Você esqueceu de informar o número")
+						return
+					}
+					if(parseInt(splitCommand[1],10) == game) {
+						mensagem.channel.send("Yata, " + mensagem.author.username + " Acertou!")
+						gameon = 0
+					}
+					else {
+						mensagem.channel.send("Chigau!")
+					}
+				}
+				else {
+					mensagem.channel.send("O jogo ainda não começou!")
+				}
+				break
+			default:
+				mensagem.channel.send("Gomenasai, não entendi o que você quer. Tente pedir ajuda pelo comando !help")
 		}
     }
 	else {
