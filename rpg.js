f9e79db9d801af79011045e8755ef47db63d45e5
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2019-05-21 01:19:13 -0300

    Update rpg.js

--- rpg.js
+++ rpg.js
@@ -108,8 +108,10 @@ client.on('message', (mensagem) => {
 			case "hangyman":
 				hangyman = 1
 				let i = Math.floor((Math.random()*hangy.length))
-				word = hangy[i]
-				mensagem.channel.send(word)
+				word = hangy[i].split('')
+				for(let i = 0;i < word.length;i++) {
+					mensagem.channel.send(word[i])
+				}
 				break
 			case "disclaimer":
 				mensagem.channel.send("Eu fui feita pelo Vinny e ainda estou em desenvolvimento, se tiver alguma duvida sobre meu funcionamento e o comando !help nao estiver ajudando, fale com ele pelo !suggestion para me ajudar a ser mais intuitiva e user-friendly!")
